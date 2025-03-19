a51f688da6ea790ed6b484fd139662f950749d7d
alicegmn <alice.eriksson.561@gmail.com>
2025-03-19 13:35:48 +0100

    Move authentication logic to separate AuthProvider

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,24 +2,14 @@ import ListContainer from "@/components/ui/listContainer";
 import SearchBar from "@/components/SearchBar";
 import Login from "./Login";
 import useAccessStore from "@/store/store";
-import useAuth from "@/helpers/useAuth";
 import SongListContainer from "@/components/ui/SongListContainer";
-import { useAuthCode } from "@/hooks/useAuthCode";
 
 export default function Home() {
-  // Använd useAuthCode-hooken för att hämta auth koden och rensa URL:en
-  const code = useAuthCode();
   const token = useAccessStore((state) => state.accessToken);
-
-  // Använd auth koden för att erhålla access token
-  useAuth(code || "");
-
-  console.log("code:", code);
-  console.log("accesstoken:", token);
 
   return (
     <div>
-      {code || token ? (
+      {token ? (
         <div>
           <h1 className="text-4xl text-white">
             Search for a song or an artist
--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,17 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../components/layouts/RootLayout";
-
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import NotFound from "../pages/NotFound";
 import ArtistId from "@/pages/ArtistId";
 import Player from "@/pages/Player";
 import Reset from "@/pages/Reset";
+import AuthProvider from "@/providers/AuthProvider";
 
 const router = createBrowserRouter([
   {
     // Huvudlayout som omsluter alla routes
-    element: <RootLayout />,
+    element: (
+      <AuthProvider>
+        <RootLayout />
+      </AuthProvider>
+    ),
     children: [
       {
         path: "/",
